4589ac8361b5ea6128b28fcdee04b03d972d4a63
mmmedina <marianomatiasmedina@gmail.com>
2024-07-17 20:00:24 -0300

    I forgot to remove the ".pures" from the methods
    i remove a space from logback that generates an error

--- src/main/scala/com/siriusxm/example/shoppingCart/ShoppingCart.scala
+++ src/main/scala/com/siriusxm/example/shoppingCart/ShoppingCart.scala
@@ -8,23 +8,24 @@ import scala.collection.mutable.ListBuffer
 import org.slf4j.Logger
 
 class ShoppingCart(productClient: ProductsClient)(implicit val log: Logger) {
-  private val items: ListBuffer[Item] = ListBuffer.empty
+  private val items: ListBuffer[Item] = ListBuffer.empty // TODO replace by cache or database
 
   def addItems(givenItems: List[Item]): IO[Unit] = {
-    log.info("Adding items...")
-    givenItems.traverse { item =>
-      productClient
-        .getByName(s"${item.title.toLowerCase}.json")
-        .redeem(
-          error => IO.pure(log.error(s"Error trying to add ${item.title} to the cart. Message: ${error.getMessage}")),
-          item =>
-            IO.pure {
-              log.info(s"Adding item ${item} to the cart")
-              items += item
-            }
-        )
+
+    log.info(s"Adding items... $givenItems")
+
+    givenItems.traverse_ { item =>
+      productClient.getByName(s"${item.title.toLowerCase}.json").attempt.flatMap {
+        case Left(error) =>
+          IO(log.error(s"Error trying to add ${item.title} to the cart. Message: ${error.getMessage}"))
+        case Right(product) =>
+          IO {
+            log.info(s"Adding item $product to the cart")
+            items += product
+          }
+      }
     }
-  }.void
+  }
 
   def listItems: IO[Map[Item, Int]] = IO {
     log.info("Getting the current items in the shopping cart")
